91a90993d3c4918a429bbe0c87e1c90d553e4c96
yunminseop <001208yun@gmail.com>
2025-03-05 13:04:59 +0900

    gymnasium FrozenLake completed

--- MS/src/gymnasium_FrozenLake.py
+++ MS/src/gymnasium_FrozenLake.py
@@ -1,6 +1,31 @@
-import gymnasium
+import gymnasium as gym
 from gymnasium.envs.registration import register
 import sys, tty, termios
+
+
+class _Getch:
+    def __call__(Self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(3)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+inkey = _Getch()
+
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+
+arrow_keys = {
+    '\x2b[A' : UP,
+    '\x2b[B' : DOWN,
+    '\x2b[C' : RIGHT,
+    '\x2b[D' : LEFT}
 
 register(
     id="FrozenLake-v3",
@@ -8,7 +33,8 @@ register(
     kwargs={"map_name" : "4x4", "is_slippery" : False}
 )
 
-env = gymnasium.make("FrozenLake-v3")
+env = gym.make("FrozenLake-v1", render_mode="human")
+env.reset()
 env.render()
 
 while True:
@@ -17,7 +43,7 @@ while True:
         print("Game Aborted!")
         break
 
-    action = arrow_keys[keys]
+    action = arrow_keys[key]
     state, reward, terminated, truncated, info = env.step(action)
 
     env.render()
